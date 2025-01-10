1acb8adb0de7424257754ff7fa451e9eeb9bc682
Fariandi Ramadhan <fariandi.ramadhann@gmail.com>
2025-01-10 14:36:53 +0700

    add:request and routing, path parameter, query parameter, and body request

---
+++ PathParameter/routes.js
@@ -0,0 +1,60 @@
+/**
+ * '/hello/{name}'
+ * parameter name harus diisi
+ * 
+ * '/hello/{name?}'
+ * parameter name optional
+ * 
+ * '/hello/{name?}/{age}'
+ * Invalid path
+ * 
+ * '/hello/{name}/{age?}'
+ * Correct path, optional parameter should go last
+ */
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            return `Hello, ${name}!`;
+        }
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+
+module.exports = routes;
---
+++ queryParameter/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+ 
+    server.route(routes);
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
+
+/**
+ * curl -X GET http://localhost:5000/hello/dicoding
+ * curl -X GET http://localhost:5000/hello
+ */
